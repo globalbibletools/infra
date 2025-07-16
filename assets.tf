6d1d69eb19ef1c5768c2c1714586687311b5485b
Adrian Rocke <me@adrianrocke.com>
2025-07-15 20:59:25 -0500

    attach subdomain to cloudfront distribution

--- assets.tf
+++ assets.tf
@@ -1,14 +1,11 @@
 resource "aws_s3_bucket" "gbt_audio" {
   bucket = "gbt-audio"
 }
-// TODO: remove after importing
-import {
-  to = aws_s3_bucket.gbt_audio
-  id = "gbt-audio"
-}
 
 resource "aws_cloudfront_distribution" "assets" {
-  enabled = false
+  enabled = true
+
+  aliases = ["assets.globalbibletools.com"]
 
   origin {
     domain_name = aws_s3_bucket.gbt_audio.bucket_regional_domain_name
@@ -30,7 +27,18 @@ resource "aws_cloudfront_distribution" "assets" {
   }
 
   viewer_certificate {
-    cloudfront_default_certificate = true
+    acm_certificate_arn = aws_acm_certificate.assets.arn
+    ssl_support_method = "sni-only"
+  }
+}
+resource "aws_route53_record" "assets" {
+  zone_id         = aws_route53_zone.main.zone_id
+  name = "assets.globalbibletools.com"
+  type = "A"
+  alias {
+    name = aws_cloudfront_distribution.assets.domain_name
+    zone_id = aws_cloudfront_distribution.assets.hosted_zone_id
+    evaluate_target_health = false
   }
 }
 
@@ -49,10 +57,6 @@ resource "aws_route53_record" "assets_ssl" {
   ttl             = 60
   type            = each.value.type
   zone_id         = aws_route53_zone.main.zone_id
-}
-moved {
-  from = aws_route53_record.assets
-  to = aws_route53_record.assets_ssl
 }
 resource "aws_acm_certificate" "assets" {
   domain_name = "assets.globalbibletools.com"
