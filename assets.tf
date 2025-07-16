e96d3a218ac3b20fbf426ccc0c3abe122d2a613d
Adrian Rocke <me@adrianrocke.com>
2025-07-15 20:38:35 -0500

    asset ssl cert

--- assets.tf
+++ assets.tf
@@ -33,3 +33,32 @@ resource "aws_cloudfront_distribution" "assets" {
     cloudfront_default_certificate = true
   }
 }
+
+resource "aws_route53_record" "assets" {
+  for_each = {
+    for dvo in aws_acm_certificate.assets.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.main.zone_id
+}
+resource "aws_acm_certificate" "assets" {
+  domain_name = "assets.globalbibletools.com"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+resource "aws_acm_certificate_validation" "assets" {
+  certificate_arn = aws_acm_certificate.assets.arn
+  validation_record_fqdns = [for record in aws_route53_record.assets : record.fqdn]
+}
