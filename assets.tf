6c6fc0c37e5d05ea0a4b584e7a4e906040e21671
Adrian Rocke <me@adrianrocke.com>
2025-07-16 20:46:18 -0500

    add access policy to new bucket for terraform

--- assets.tf
+++ assets.tf
@@ -6,6 +6,34 @@ resource "aws_s3_bucket" "assets" {
   bucket = "gbt-assets"
 }
 
+
+data "aws_iam_policy_document" "assets_cloudfront_access" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = ["cloudfront.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.assets.arn}/*"
+    ]
+
+    condition {
+      test = "StringEquals"
+      variable = "AWS:SourceArn"
+      values = [aws_cloudfront_distribution.assets.arn]
+    }
+  }
+}
+resource "aws_s3_bucket_policy" "assets" {
+  bucket = aws_s3_bucket.assets.id
+  policy = data.aws_iam_policy_document.assets_cloudfront_access.json
+}
+
 resource "aws_cloudfront_distribution" "assets" {
   enabled = true
 
