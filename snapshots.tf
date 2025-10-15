1c7646203d0414f76ff489ebf09a18d30d528b3a
Adrian Rocke <me@adrianrocke.com>
2025-10-14 19:31:19 -0500

    enable get object access for local snapshots

--- snapshots.tf
+++ snapshots.tf
@@ -9,6 +9,7 @@ data "aws_iam_policy_document" "snapshots_local_access" {
 
     actions = [
         "s3:PutObject",
+        "s3:GetObject",
         "s3:CreateMultipartUpload",
         "s3:UploadPart",
         "s3:CompleteMultipartUpload",
