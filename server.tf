d3deb9bb1236fb1d4e1ab842836794c36d6738b2
Adrian Rocke <me@adrianrocke.com>
2026-07-04 13:13:36 -0500

    correct env var

--- server.tf
+++ server.tf
@@ -111,7 +111,7 @@ resource "aws_apprunner_service" "server" {
           OPENAI_KEY                   = var.openai_key
           FATHOM_ID                    = var.fathom_id
           VITE_FATHOM_ID               = var.fathom_id
-          JOB_QUEUE_URL                = aws_sqs_queue.jobs_light.url
+          JOB_QUEUE_LIGHT_URL                = aws_sqs_queue.jobs_light.url
           BIBLE_SYSTEMS_API_KEY        = var.global_bible_systems_api_key
         }
       }
