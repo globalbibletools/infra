be1470bd25ae84b0509695490ea6b11a2da57efb
Adrian Rocke <me@adrianrocke.com>
2026-07-04 22:14:24 -0500

    queue and log group for heavy queue

--- job_queue_heavy.tf
+++ job_queue_heavy.tf
@@ -22,3 +22,30 @@ resource "aws_ecr_lifecycle_policy" "job_worker_heavy" {
   policy     = data.aws_ecr_lifecycle_policy_document.job_worker_heavy.json
 }
 
+resource "aws_sqs_queue" "jobs_heavy" {
+  name                       = "jobs_heavy"
+  visibility_timeout_seconds = 600
+}
+
+resource "aws_sqs_queue" "jobs_heavy_dlq" {
+  name                      = "jobs_heavy_dlq"
+  message_retention_seconds = 1209600 # 14 days
+
+  redrive_allow_policy = jsonencode({
+    redrivepermission = "byqueue",
+    sourcequeuearns   = [aws_sqs_queue.jobs_heavy.arn]
+  })
+}
+
+resource "aws_sqs_queue_redrive_policy" "jobs_heavy" {
+  queue_url = aws_sqs_queue.jobs_heavy.id
+
+  redrive_policy = jsonencode({
+    deadlettertargetarn = aws_sqs_queue.jobs_heavy_dlq.arn
+    maxreceivecount     = 1
+  })
+}
+
+resource "aws_cloudwatch_log_group" "job_worker_heavy_ecs" {
+  name = "/aws/lambda/job_worker_heavy"
+}
